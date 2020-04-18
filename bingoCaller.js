f653d59cc6751d3f6298a9081ccbb88dcb1a2b7b
hokieknight <dale.mrotek@gmail.com>
2020-04-18 18:29:12 -0400

    first commit

---
+++ bingoCaller.js
@@ -0,0 +1,43 @@
+
+var tableRef = document.getElementById('bingoTable');
+
+for (var i = 1; i < 16; i++) {
+    var newRow = tableRef.insertRow();
+
+    for (var j = 0; j < 5; j++) {
+        var cellValue = i + (15 * j);
+
+        var newCell  = newRow.insertCell(j);
+        newCell.id = 'cell' + cellValue;
+        var newText  = document.createTextNode('' + cellValue);
+        newCell.appendChild(newText);
+    }
+}
+
+
+$(".callButton").click(function() {
+
+    var done = false;
+    var callNum = 0;
+    while (!done) {
+        callNum = Math.floor(Math.random()*75)+1;
+
+        var selected = document.querySelector('#cell'+callNum).style.backgroundColor === "grey";
+        if (!selected)
+            done = true;
+    }
+
+    var callLetter = 'O';
+    if (callNum < 16) callLetter = 'B';
+    else if (callNum < 31) callLetter = 'I';
+    else if (callNum < 46) callLetter = 'N';
+    else if (callNum < 61) callLetter = 'G';
+
+    document.querySelector("#callValue").innerHTML = callLetter + ' - ' + callNum;
+    document.querySelector('#cell'+callNum).style.textDecoration = "line-through";
+    document.querySelector('#cell'+callNum).style.backgroundColor = "grey";
+});
+  
+
+
+
---
+++ bingoCard.js
@@ -0,0 +1,46 @@
+
+var tableRef = document.getElementById('bingoTable');
+
+for (var i = 0; i < 5; i++) {
+    var newRow = tableRef.insertRow();
+
+    for (var j = 0; j < 5; j++) {
+        var randNum = 0;
+        
+        if (i == 2 && j == 2)
+            randNum = 'Free';
+        else {
+            var done = false;
+            while (!done) {
+                randNum = Math.floor(Math.random()*15)+ 1 + (15 * j);
+                var selected = document.querySelector('#cell'+randNum);
+                if (selected == null)
+                    done = true;
+            }
+        }
+
+        var newCell  = newRow.insertCell(j);
+        newCell.id = 'cell' + randNum;
+        newCell.setAttribute('class', 'bingoCell');
+        var newText  = document.createTextNode('' + randNum);
+        newCell.appendChild(newText);
+    }
+}
+
+
+$(".bingoCell").click(function() {
+    if (this.style.backgroundColor === "grey")
+    {
+        this.style.textDecoration = "";
+        this.style.backgroundColor = "white";
+    } 
+    else
+    {
+        this.style.textDecoration = "line-through";
+        this.style.backgroundColor = "grey";
+    }
+});
+  
+
+
+
